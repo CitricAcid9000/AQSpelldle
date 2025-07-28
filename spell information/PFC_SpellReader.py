20463053fecd20e3ea04491683097e39a0cf0730
CitricAcid9000 <sean.steuber@gmail.com>
2025-07-28 16:06:39 -0500

    set up for JSON file

---
+++ spell information/PFC_SpellReader.py
@@ -0,0 +1,112 @@
+'''
+    This was made By Macarkeys here https://github.com/Macarkeys/PFC_SpellReader/tree/main and was updated by Me
+'''
+
+''' What needs to be done:
+1. make each spell an idependant dictionary with the spell name as the key and the spell information as the value
+2. Make sure that the spell names are not duplicated in the same document by condesning them and combining them into a single dictionary
+   - This is done by the getSpellDicts function, which takes in a spell table
+'''
+# combines paragraphs from a list of paragraphs
+def collatPara(paragraphList):
+  strPara = ''
+  for i in paragraphList:
+    strPara += cleanText(i.text)
+  return strPara
+def cleanText(textInput: str):
+  textInput = textInput.strip()
+  return textInput
+# Cleans the lists in spell table and preps. Removes the little header row and changes the empty row at bottom of every spell to be the description, removing the third column.
+def cleanSpellTable(SpellTable):
+  cleanedSpells = [[],[]]
+  for i in range(len(SpellTable[0])):
+    if (SpellTable[0][i],SpellTable[1][i]) != ('','') and ('Description' not in SpellTable[2][i].strip()):
+      cleanedSpells[0].append(SpellTable[0][i])
+      cleanedSpells[1].append(SpellTable[1][i])
+    elif SpellTable[0][i].strip() == '' and SpellTable[0][i-1].strip() != '':
+      cleanedSpells[0].append('Description')
+      cleanedSpells[1].append(SpellTable[2][i-6])
+  return cleanedSpells
+# Gets the indexes of each spell in each spell table
+# reform following two functions to not take spells and their dictionaries by 8 but until they reach the text spell title
+def getSpellIndexes(SpellTable):
+  spellIndex = []
+  x = 0
+  i = 0 
+  for i in range(len(SpellTable[0])):
+    if '\u2013' in SpellTable[0][i] or '\u002D' in SpellTable[0][i]:
+      print(SpellTable[0][i])
+      if x != 0:
+        spellIndex.append((i-x,x))
+      x = 0
+    x += 1
+  spellIndex.append((len(SpellTable[0])-x, x))
+  return spellIndex
+# gets the spells for that spell table as a dictionary
+def getSpellDicts(SpellTable):
+  spellListDict = {}
+  for i in getSpellIndexes(SpellTable):
+    keyL = SpellTable[0][i[0]+1:i[0]+i[1]]
+    valL = SpellTable[1][i[0]+1:i[0]+i[1]]
+    spellDict = {}
+    for j in range(len(keyL)):
+      spellDict[keyL[j]] = valL[j]
+    spellListDict[SpellTable[0][i[0]]] = spellDict
+  return spellListDict
+# This is the main function that is taking in a table and outputing a fully cleaned and organized spell table as a dictionary. Ex: SpellTable['1 - Concern']
+def organizeTable(docTable):
+  colLen = len(list(docTable.columns))
+  rowLen = len(list(docTable.rows))
+  docTableCells = [docTable.column_cells(i) for i in range(colLen)]
+  SpellTable = []
+  for i in range(colLen):
+    SpellTable.append([docTableCells[i][j].paragraphs for j in range(rowLen)])
+    SpellTable[i] = list(map(collatPara,SpellTable[i]))
+  SpellTable = cleanSpellTable(SpellTable)
+  SpellTable = getSpellDicts(SpellTable)
+  return SpellTable
+
+def getDocsSpells(doc):
+  # creating empty lists to one day combine into a dictionary
+  spellGroupTitles = []
+  spellGroups = []
+  tableing = [False, -1]
+  docSpellName = None
+  for element in doc.element.body:
+    # Finds the headers of the spell groups and signifies when to start collecting tables
+    if isinstance(element, CT_P):
+      if element.style == 'Heading5':
+        tableing[0] = True
+        tableing[1] += 1
+        spellGroupTitles.append(element.text)
+        spellGroups.append({})
+      if (element.style == 'Heading2' or element.style == 'Title') and not docSpellName:
+        docSpellName = element.text
+    # checks if element is a table, then accesses it as a table (?!?!), then combines the dictionary already present (empty dict if first) with the new one
+    if isinstance(element, CT_Tbl) and tableing[0]:
+      table = Table(element, doc)
+      if len(table._cells) > 10:
+        spellGroups[tableing[1]] = {**spellGroups[tableing[1]], **organizeTable(table)}
+  # Creating a dictionary from the spell group titles (keys) and spell group dictionaries (values)
+  docSpellDict = {}
+  for i in range(len(spellGroupTitles)):
+    docSpellDict[spellGroupTitles[i]] = spellGroups[i]
+  return docSpellDict, docSpellName
+
+if __name__ == "__main__":
+  from docx import Document
+  from docx.table import Table
+  from docx.oxml.text.paragraph import CT_P
+  from docx.text.paragraph import Paragraph
+  from docx.oxml.table import CT_Tbl
+  import json
+  import os
+  #opening the document as a docx.Document object
+  allSpells = {}
+  for file in os.listdir('spellFolder'):
+    docSpell = getDocsSpells(Document("spellFolder\\"+file))
+    allSpells[docSpell[1]] = docSpell[0]
+  # next steps is to do this for every spell document and make sure there is no errors then combine them into a big dictionary so bigDict['Orus']['Love']['1 - Concern'] (- not –)
+  #print(docSpellDict['Hate']['10 – Malevolence']) # currently an issue is present where – is present in spell names not -. They may look similiar but not the same
+  with open('pfc-elemental-divine-psionic-spells.json', 'w') as outfile:
+    json.dump(allSpells, outfile, indent=4)
